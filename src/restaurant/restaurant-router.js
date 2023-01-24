37a0a3a58ba9f94787613e23e1a07981d96a5fea
RD <renatafd717@gmail.com>
2023-01-23 21:26:42 -0600

    changed route away from herokku to new server

--- src/restaurant/restaurant-router.js
+++ src/restaurant/restaurant-router.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const xss = require('xss');
-const restValidationService = require('./restaurant');
-const path = require('path');
+const express = require("express");
+const xss = require("xss");
+const restValidationService = require("./restaurant");
+const path = require("path");
 const restRouter = express.Router();
 const jsonBodyParser = express.json();
-const logs = require('../logs');
+const logs = require("../logs");
 
 //XSS TO PROTECT AGAINST SCRIPT ATTACKS
 const serialRest = (rest) => ({
@@ -16,7 +16,7 @@ const serialRest = (rest) => ({
 });
 
 restRouter
-  .route('/register')
+  .route("/register")
   //RESTAURANT REGISTER
   .post(jsonBodyParser, (req, res, next) => {
     const trimRest = {
@@ -30,53 +30,44 @@ restRouter
     // trimRest.name = trimRest.name.trim().replace(/\s+/g, ' ');
     // trimRest.phone = trimRest.phone.replace(/\D/g, '');
     // trimRest.username = trimRest.username.replace(/\s/g, '');
-        
-    
+
     //VALIDATION FOR NAME AND PRICE [REQUIRED FIELDS]
-    for (const field of ['username', 'password', 'name', 'phone'])
+    for (const field of ["username", "password", "name", "phone"])
       if (!trimRest[field]) {
         logs.error(`Restaurant ${field} is required`);
-        return res
-          .status(400)
-          .json({ error: `The ${field} is required.` });
+        return res.status(400).json({ error: `The ${field} is required.` });
       }
 
     //CONST CALLING PASSWORD VALIDATION
     const passError = restValidationService.passValidation(trimRest.password);
     if (passError) {
       logs.error(passError);
-      return res
-        .status(400)
-        .json({ error: passError });
+      return res.status(400).json({ error: passError });
     }
-    
+
     //CONST CALLING NAME VALIDATION
     const nameError = restValidationService.nameValidation(trimRest.name);
     if (nameError) {
       logs.error(nameError);
-      return res
-        .status(400)
-        .json({ error: nameError });
+      return res.status(400).json({ error: nameError });
     }
 
     //CONST CALLING PHONE VALIDATION
     const phoneError = restValidationService.phoneValidation(trimRest.phone);
     if (phoneError) {
       logs.error(phoneError);
-      return res
-        .status(400)
-        .json({ error: phoneError });
+      return res.status(400).json({ error: phoneError });
     }
 
     //ADDING RESTAURANT VALIDATION
     restValidationService
-      .checkRestLogin(req.app.get('db'), trimRest.username)
+      .checkRestLogin(req.app.get("db"), trimRest.username)
       .then((validRest) => {
         if (validRest) {
-          logs.error('Restaurant username already exists.');
+          logs.error("Restaurant username already exists.");
           return res
             .status(400)
-            .json({ error: 'Username already exists. Try again.' });
+            .json({ error: "Username already exists. Try again." });
         }
         return restValidationService
           .passHash(trimRest.password)
@@ -84,7 +75,7 @@ restRouter
             trimRest.password = hashedPass;
 
             return restValidationService
-              .addRest(req.app.get('db'), trimRest)
+              .addRest(req.app.get("db"), trimRest)
               .then((rest) => {
                 logs.info(
                   `Restaurant created successfully. The restaurant id is: ${rest.id}.`
@@ -93,7 +84,7 @@ restRouter
                   .status(201)
                   .location(
                     path.posix.join(
-                      'https://dry-fjord-49769.herokuapp.com/',
+                      "https://web-production-ad4e.up.railway.app/",
                       `/restaurant/${rest.id}`
                     )
                   )
@@ -105,13 +96,13 @@ restRouter
   });
 
 restRouter
-  .route('/restaurant/:id')
+  .route("/restaurant/:id")
   //DELETES A RESTAURANT WITH ID
   .delete(jsonBodyParser, (req, res, next) => {
     const { id } = req.params;
 
     restValidationService
-      .deleteRestaurant(req.app.get('db'), id)
+      .deleteRestaurant(req.app.get("db"), id)
       .then(() => {
         logs.info(`Restaurant with id ${id} deleted.`);
         res.status(204).end();
@@ -131,63 +122,66 @@ restRouter
     //trimUpdateRest.phone = trimUpdateRest.phone.replace(/\D/g, '');
 
     //VALIDATION FOR NAME AND PRICE [REQUIRED FIELDS]
-    for (const field of ['name', 'phone'])
+    for (const field of ["name", "phone"])
       if (!trimUpdateRest[field]) {
         logs.error(`Restaurant ${field} is required`);
-        return res
-          .status(400)
-          .json({ error: `The ${field} is required.` });
+        return res.status(400).json({ error: `The ${field} is required.` });
       }
 
     //CONST CALLING NAME VALIDATION
     const nameError = restValidationService.nameValidation(trimUpdateRest.name);
-    if(nameError){
+    if (nameError) {
       logs.error(nameError);
-      return res
-        .status(400)
-        .json({ error: nameError });
+      return res.status(400).json({ error: nameError });
     }
-    
+
     //CONST CALLING PHONE VALIDATION
-    const phoneError = restValidationService.phoneValidation(trimUpdateRest.phone);
+    const phoneError = restValidationService.phoneValidation(
+      trimUpdateRest.phone
+    );
     if (phoneError) {
       logs.error(phoneError);
-      return res
-        .status(400)
-        .json({ error: phoneError });
+      return res.status(400).json({ error: phoneError });
     }
 
     //UPDATE RESTAURANT VALIDATION
     restValidationService
-      .updateRestaurant(req.app.get('db'), trimUpdateRest.id, trimUpdateRest.name, trimUpdateRest.phone)
+      .updateRestaurant(
+        req.app.get("db"),
+        trimUpdateRest.id,
+        trimUpdateRest.name,
+        trimUpdateRest.phone
+      )
       .then(() => {
         if (trimUpdateRest.name) {
-          logs.info(`Restaurant name ${trimUpdateRest.name} was updated successfully.`);
+          logs.info(
+            `Restaurant name ${trimUpdateRest.name} was updated successfully.`
+          );
           res.status(204).end();
         }
         if (trimUpdateRest.phone) {
-          logs.info(`Restaurant phone ${trimUpdateRest.phone} was updated successfully.`);
+          logs.info(
+            `Restaurant phone ${trimUpdateRest.phone} was updated successfully.`
+          );
           res.status(204).end();
         }
       })
-      .catch(err => {
+      .catch((err) => {
         res.status(409).json({ error: err });
       });
   });
 
 restRouter
-  .route('/restaurant-dish-list/:id')
+  .route("/restaurant-dish-list/:id")
   //GETS DISHES FROM A SPECIFIC REST. ID
   .get((req, res, next) => {
     const { id } = req.params;
 
     restValidationService
-      .showRestaurantDishesByID(req.app.get('db'), id)
+      .showRestaurantDishesByID(req.app.get("db"), id)
       .then((restID) => {
         logs.info(`Returned all dishes from Restaurant ${id} successfully.`);
-        res
-          .status(200)
-          .json(restID);
+        res.status(200).json(restID);
       })
       .catch(next);
   })
@@ -197,9 +191,11 @@ restRouter
     const restaurant_id = req.params.id;
 
     restValidationService
-      .deleteRestaurantDishes(req.app.get('db'), dish_id, restaurant_id)
+      .deleteRestaurantDishes(req.app.get("db"), dish_id, restaurant_id)
       .then(() => {
-        logs.info(`Dish id ${dish_id} from Restaurant id ${restaurant_id} deleted.`);
+        logs.info(
+          `Dish id ${dish_id} from Restaurant id ${restaurant_id} deleted.`
+        );
         res.status(204).end();
       })
       .catch(next);
